kaffuchino233/hardware_oplus

VibratorExt: Add support for corvette
Change-Id: I0f14ac3a7a36e86ed62940c4017f64cebafaa8a8

aidl/vibratorExt/include/Constant.h
@@ -62,7 +62,7 @@ static const LevelRangeConfig UNIFIED_LEVEL_RANGE_CONFIG = {
     }
 };
 
-static const LevelRangeConfig LEVEL_RANGE_CONFIG_OP11_12_13 = {
+static const LevelRangeConfig LEVEL_RANGE_CONFIG_OP11_12_13_ACE3P = {
     .alarmCallLevelRange = {
         .maxLevel = 5,
         .defaultLevel = 4

sunOS-ice/android_hardware_oplus

aidl/vibratorExt/include/Constant.h
@@ -62,7 +62,7 @@ static const LevelRangeConfig UNIFIED_LEVEL_RANGE_CONFIG = {
     }
 };
 
-static const LevelRangeConfig LEVEL_RANGE_CONFIG_OP11_12_12R_13 = {
+static const LevelRangeConfig LEVEL_RANGE_CONFIG_OP11_12_12R_13_ACE3P = {
     .alarmCallLevelRange = {
         .maxLevel = 5,
         .defaultLevel = 4